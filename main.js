7f0b61e5baf99b1a29e64596ad9d14f0a9b70da0
c mulligan <gateless@protonmail.com>
2019-09-25 18:52:58 -0400

    add semicolons to help bad browser engines

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const init = (init,cb=null)=>(cb && cb(init)||true)&&({get:()=>init,set:(next)=>(cb)?cb(init=next):init=next})
+const init = (init,cb=null)=>(cb && cb(init)||true)&&({get:()=>init,set:(next)=>(cb)?cb(init=next):init=next});
 
 const acts = (obj={}) => {
   const acts = init({...obj})
@@ -6,55 +6,55 @@ const acts = (obj={}) => {
   return ({
     sub:(act,fn)=>acts.set(Object.assign({},acts.get(),{[act]:fn})),
     pub:(act)=>acts.get()[act]
-  })
-}
+  });
+};
 
-const pipe = (...fns) => (x) => fns.reduce((a,c)=>c(a),x)
+const pipe = (...fns) => (x) => fns.reduce((a,c)=>c(a),x);
 
-const grab = (id) => document.getElementById(id)
+const grab = (id) => document.getElementById(id);
 
-const edit_prop = (prop) => (comp) => (v) => (comp[prop]=v) && comp
+const edit_prop = (prop) => (comp) => (v) => (comp[prop]=v) && comp;
 
-const put = (component) => (host) => host.append(component) || host
+const put = (component) => (host) => host.append(component) || host;
 
 const empty = (host) => (!host.firstChild)
       ? host
-      : host.removeChild(host.firstChild) && empty(host)
+      : host.removeChild(host.firstChild) && empty(host);
 
 const button = (txt) => {
-  const el = document.createElement('button')
+  const el = document.createElement('button');
   el.textContent = txt;
-  return el
-}
+  return el;
+};
 
 const tracker = (z,id,img) => {
-  const container = document.createElement('div')
-  const token = document.createElement('div')
-  const count = document.createElement('div')
+  const container = document.createElement('div');
+  const token = document.createElement('div');
+  const count = document.createElement('div');
 
-  const state = init(z,edit_prop('textContent')(count))
-  const inc = pipe(state.get,(x)=>x+1,state.set) 
-  const dec = pipe(state.get,(x)=>(x===0)?0:x-1,state.set) 
+  const state = init(z,edit_prop('textContent')(count));
+  const inc = pipe(state.get,(x)=>x+1,state.set);
+  const dec = pipe(state.get,(x)=>(x===0)?0:x-1,state.set); 
 
-  const actions = acts({inc,dec})
+  const actions = acts({inc,dec});
   
-  const up = button('+')
-  const down = button('-')
+  const up = button('+');
+  const down = button('-');
 
-  up.addEventListener('click',actions.pub('inc'))
-  down.addEventListener('click',actions.pub('dec'))
+  up.addEventListener('click',actions.pub('inc'));
+  down.addEventListener('click',actions.pub('dec'));
 
   token.style.backgroundImage = `url(${img})`;
   count.style.color = `${id}`;
   count.style.fontSize = '3em';
 
-  edit_prop('id')(container)(id)
-  edit_prop('className')(container)('tracker')
+  edit_prop('id')(container)(id);
+  edit_prop('className')(container)('tracker');
 
-  return pipe(put(token),put(up),put(count),(id==='storm')?(x)=>x:put(down))(container)
+  return pipe(put(token),put(up),put(count),(id==='storm')?(x)=>x:put(down))(container);
 }
 
-const fizzle = button('fizzle')
+const fizzle = button('fizzle');
 
 const build = (host) => pipe(
   put(fizzle),
@@ -64,8 +64,9 @@ const build = (host) => pipe(
   put(tracker(0,'blue','assets/blue.svg')),
   put(tracker(0,'green','assets/green.svg')),
   put(tracker(0,'white','assets/white.svg')),
-)(host)
+)(host);
 
-fizzle.addEventListener('click',()=>empty(grab('container'))&&build(grab('container')))
+fizzle.addEventListener('click',()=>empty(grab('container'))&&build(grab('container')));
 
-build(grab('container'))
+build(grab('container'));
+
